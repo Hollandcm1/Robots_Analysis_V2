2798a83a3762e8a451135d8e41bec54e597a4b69
Hollandcm1 <chris_h1996@hotmail.com>
2024-10-14 19:02:56 -0300

    simple LME v2 (not working)

--- R_targets_scripts/simple_LMEs.R
+++ R_targets_scripts/simple_LMEs.R
@@ -25,18 +25,20 @@ average_by_trial$visual <- as.factor(average_by_trial$visual)
 average_by_trial$haptic <- as.factor(average_by_trial$haptic)
 average_by_trial$participant <- as.factor(average_by_trial$participant)
 
+tmp_exp1_trial <- average_by_trial
+
 # average by condition
-# average_by_condition <- average_by_trial %>%
-#   group_by(participant, haptic, visual, condition_nums, experiment) %>%
-#   summarise(average_time_through_maze = mean(time_through_maze, na.rm = TRUE),
-#             average_path_length = mean(path_length, na.rm = TRUE),
-#             average_velocity = mean(average_velocity, na.rm = TRUE),
-#             average_proximity = mean(average_proximity, na.rm = TRUE),
-#             average_force = mean(average_force, na.rm = TRUE))
+average_by_condition <- average_by_trial %>%
+  group_by(participant, haptic, visual, condition_nums, experiment) %>%
+  summarise(average_time_through_maze = mean(time_through_maze, na.rm = TRUE),
+            average_path_length = mean(path_length, na.rm = TRUE),
+            average_velocity = mean(average_velocity, na.rm = TRUE),
+            average_proximity = mean(average_proximity, na.rm = TRUE),
+            average_force = mean(average_force, na.rm = TRUE))
 
 # store for later
-# tmp_exp1 <- average_by_condition
-tmp_exp1 <- average_by_trial
+tmp_exp1 <- average_by_condition
+#tmp_exp1 <- average_by_trial
 
 
 data <- data_exp2
@@ -53,24 +55,29 @@ average_by_trial$visual <- as.factor(average_by_trial$visual)
 average_by_trial$haptic <- as.factor(average_by_trial$haptic)
 average_by_trial$participant <- as.factor(average_by_trial$participant)
 
+tmp_exp2_trial <- average_by_trial
+
 # # Average by condition
-# average_by_condition <- average_by_trial %>%
-#   group_by(participant, haptic, visual, condition_nums, experiment) %>%
-#   summarise(average_time_through_maze = mean(time_through_maze, na.rm = TRUE),
-#             average_path_length = mean(path_length, na.rm = TRUE),
-#             average_velocity = mean(average_velocity, na.rm = TRUE),
-#             average_proximity = mean(average_proximity, na.rm = TRUE),
-#             average_force = mean(average_force, na.rm = TRUE))
-# 
-# # store for later
-# tmp_exp2 <- average_by_condition
-tmp_exp2 <- average_by_trial
+average_by_condition <- average_by_trial %>%
+  group_by(participant, haptic, visual, condition_nums, experiment) %>%
+  summarise(average_time_through_maze = mean(time_through_maze, na.rm = TRUE),
+            average_path_length = mean(path_length, na.rm = TRUE),
+            average_velocity = mean(average_velocity, na.rm = TRUE),
+            average_proximity = mean(average_proximity, na.rm = TRUE),
+            average_force = mean(average_force, na.rm = TRUE))
+
+# store for later
+tmp_exp2 <- average_by_condition
+#tmp_exp2 <- average_by_trial
 
 # combine
 all_data <- rbind(tmp_exp1, tmp_exp2)
+# average_by_trial <- all_data
 average_by_condition <- all_data
 
-
+################################################################################
+################################## Pre Averaged ################################
+################################################################################
 
 ################################
 ### Experiment 1 Simple LMEs ###
@@ -131,6 +138,73 @@ model <- lmer(average_force ~ visual*haptic + (1|participant), data = tmp_exp2)
 summary(model)
 tab_model(model)
 flexplot(data = tmp_exp2, average_force ~ visual + haptic)
+
+
+
+################################################################################
+################################ By Trial Version ##############################
+################################################################################
+
+################################
+### Experiment 1 Simple LMEs ###
+################################
+
+model <- lmer(time_through_maze ~ visual*haptic + (1|participant), data = tmp_exp1_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp1_trial, time_through_maze ~ visual + haptic)
+
+model <- lmer(path_length ~ visual*haptic + (1|participant), data = tmp_exp1_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp1_trial, path_length ~ visual + haptic)
+
+model <- lmer(average_velocity ~ visual*haptic + (1|participant), data = tmp_exp1_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp1_trial, average_velocity ~ visual + haptic)
+
+model <- lmer(average_proximity ~ visual*haptic + (1|participant), data = tmp_exp1_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp1_trial, average_proximity ~ visual + haptic)
+
+model <- lmer(average_force ~ visual*haptic + (1|participant), data = tmp_exp1_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp1_trial, average_force ~ visual + haptic)
+
+
+################################
+### Experiment 2 Simple LMEs ###
+################################
+
+model <- lmer(time_through_maze ~ visual*haptic + (1|participant), data = tmp_exp2_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp2_trial, time_through_maze ~ visual + haptic)
+
+model <- lmer(path_length ~ visual*haptic + (1|participant), data = tmp_exp2_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp2_trial, path_length ~ visual + haptic)
+
+model <- lmer(average_velocity ~ visual*haptic + (1|participant), data = tmp_exp2_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp2_trial, average_velocity ~ visual + haptic)
+
+model <- lmer(average_proximity ~ visual*haptic + (1|participant), data = tmp_exp2_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp2_trial, average_proximity ~ visual + haptic)
+
+model <- lmer(average_force ~ visual*haptic + (1|participant), data = tmp_exp2_trial)
+summary(model)
+tab_model(model)
+flexplot(data = tmp_exp2_trial, average_force ~ visual + haptic)
+
+
 
 
 
